1561dcfde6248d03ccb088b43c7677b59b5683ed
Md Tazri <mdtazri@gmail.com>
2020-12-01 23:37:50 +0600

    I create new folder which name is chapter two.Then starting revise.My first revise topic is variable.But it simple topic.When I complete chapter two then I write next commit.

---
+++ chapter two/variable.js
@@ -0,0 +1,18 @@
+//variable like a container store somthing
+
+var  name = 'Tazri';
+
+var age = 19
+
+console.log('I am '+name);
+console.log('I am '+age+'year old');
+
+var numberOne = 10;
+
+var numberTwo = 20;
+
+var result = numberOne + numberTwo;
+console.log(result);
+console.log(numberThree);
+var numberThree = 1000;
+console.log(numberThree);
