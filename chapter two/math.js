db84140b296c9c1716d5143eda56218d17a3871d
Md Tazri <MdTazri@MdTazri>
2020-12-06 18:38:54 +0600

    I Complate revise chapter two

---
+++ chapter two/math.js
@@ -0,0 +1,17 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+console.log(Math.abs(34));//34
+console.log(Math.floor(34.5));//35
+console.log(Math.ceil(32.3));//33
+console.log(Math.round(33.5));//34
+console.log(Math.round(34.6));//35
+console.log(Math.min(34,4,56,-4,56,3,2));//-4
+console.log(Math.min(34,4,2));//2
+console.log(Math.max(3,45,67,5));//45
+console.log(Math.pow(3,3));//27
+console.log(Math.pow(3,2));//9
+console.log(Math.sqrt(16))//4
+console.log(Math.random());
+console.log(Math.random()*45+1);
+console.log(Math.round(Math.random()*50+1));
